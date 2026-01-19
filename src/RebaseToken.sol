ec253e1341030af6cf1b6ff6476d87bad6c57edc
cypherpulse <johnbradill67@gmail.com>
2026-01-19 07:15:23 +0300

    Implementing a cross-chain rebase token that dynamically adjusts supply across blockchains, enabling seamless value transfers. Built with Solidity for robust smart contracts, Foundry for efficient development, testing, and deployment, and Chainlink's Cross-Chain Interoperability Protocol (CCIP) for secure, decentralized cross-chain messaging and interoperability. Includes comprehensive test suites, deployment scripts for multiple chains, and documentation. - src/RebaseToken.sol

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MIT
 
-
-
 // Layout of Contract:
 // version
 // imports
@@ -35,8 +33,15 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @notice Each user will have their own interest rate that is the global interest rate at the time of deposit.
  */
 contract RebaseToken is ERC20 {
-    constructor() ERC20("Rebase Token","RBT"){
+    //ERRORS//
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
+    event InterestRateSet(uint256 newInterestRate);
 
-    }
+    //STATE VARIABLES//
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    uint256 private s_interestRate = 5e10;
+    mapping(address => uint256) private s_userInterestRate;
+    mapping(address => uint256) private s_userLastUpdatedTimestamp;
 
+    constructor() ERC20("Rebase Token", "RBT") {}
 }
