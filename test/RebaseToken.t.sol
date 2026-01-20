50fc29d9764fb3bafa52cd4d622547d31beffc1a
cypherpulse <johnbradill67@gmail.com>
2026-01-20 09:20:30 +0300

    Writing tests for  rebase token that dynamically adjusts supply across blockchains, enabling seamless value transfers. Built with Solidity for robust smart contracts, Foundry for efficient development, testing, and deployment, and Chainlink's Cross-Chain Interoperability Protocol (CCIP) for secure, decentralized cross-chain messaging and interoperability. Includes comprehensive test suites, deployment scripts for multiple chains, and documentation. - test/RebaseToken.t.sol

--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -1,3 +1,16 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.24;
+
+
+import {Test, console} from "forge-std/Test.sol";
+import {RebaseToken} from "../src/RebaseToken.sol";
+import {Vault} from "../src/Vault.sol";
+import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
+
+contract RebaseTokenTest is Test {
+    RebaseToken private rebaseToken;
+    Vault private vault;
+
+    
+}
